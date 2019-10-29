185bfe533ffb75b185cad838ec76f485bda1af17
Matt Everett <matt.g.everett@gmail.com>
2019-10-29 19:26:17 +0000

    Add openebs and persistence for elasticsearch.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+OPENEBS_CHART_VERSION='1.3.1'
 RABBIT_CHART_VERSION={{rabbit-chart-version}}
 FLUENTD_CHART_VERSION='1.10.0'
 ELASTICSEARCH_CHART_VERSION='7.2.1-0'
@@ -12,6 +13,7 @@ shouldClean=0
 shouldDeploy=1
 
 function clean () {
+    helm del --purge openebs
     helm del --purge rabbitmq-ha
     helm del --purge fluentd
     helm del --purge elasticsearch
@@ -21,6 +23,9 @@ function clean () {
 
 function deploy() {
     helm repo add elastic https://helm.elastic.co
+    helm repo up
+
+    helm install stable/openebs --wait --name openebs --version ${OPENEBS_CHART_VERSION} --namespace ${namespace} --values ${scriptDir}/../helm/values/openebs.yaml
 
     kubectl apply --recursive -f ${scriptDir}/../k8s
     helm install stable/rabbitmq-ha --name rabbitmq-ha --version ${RABBIT_CHART_VERSION} --namespace ${namespace} --values ${scriptDir}/../helm/values/rabbitmq-ha.yaml
